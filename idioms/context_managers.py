6f9882867ba415322338c4f5f931334104821ae6
nathanokane80 <nathanokane80@gmail.com>
2025-06-24 13:08:03 +0100

    New working context manager using contextlib

--- idioms/context_managers.py
+++ idioms/context_managers.py
@@ -2,13 +2,16 @@ from contextlib import contextmanager
 from time import perf_counter
 import os
 
+#Created on 23/06/25 by Nathan O'Kane
+#Last Modified 24/06/25 by Nathan O'Kane
+
 
 #Basic context manager, which does the same job as the with function for opening files
 #Returns a file object and allows you to use any file functions that you would use normally
 
-#
+#----------------------------------------Context Manager #1---------------------------------------#
 
-
+"""
 class ContextManager:
     def __init__(self, name):
         self.fileName = name
@@ -38,4 +41,30 @@ class ContextManager:
 with ContextManager("../read-files/words.txt") as myfile:
     lines=myfile.read()
 
+"""
+#--------------------------------------Context Manager #2 (using contextlib)------------------------#
+
+#This context manager simply switches the current directory to the supplied directory and then back again,
+#Once some action has been performed in the with block
+#Here, it writes to a file, in the new context provided
+
+@contextmanager
+def change_dir(target_path):
+    changed_dir = False
+    original_path = os.getcwd()
+    os.chdir(target_path)
+    try:
+        
+        yield
+    finally:
+        if changed_dir:
+            os.chdir(original_path)
+
+print("Directory before: ", os.getcwd())
+with change_dir("../read-files/"):
+    with open("text-from-context-manager.txt", "w") as f:
+        f.write("Hello, this was created from the context manager")
+    print("In with block it is: ", os.getcwd())
+
+print("And after it is: ", os.getcwd())
     
