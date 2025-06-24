c56eb9a7b1b33fbb647ffad66a8d07a81051c57b
nathanokane80 <nathanokane80@gmail.com>
2025-06-24 16:54:53 +0100

    Added file for use of 'slots' in python

---
+++ idioms/slots.py
@@ -0,0 +1,23 @@
+#This file was created by Nathan O'Kane on 24/06/24
+#Last edited by Nathan O'Kane on 24/06/24
+
+#Below is an example of a python's slots, which allows restriction of a classes attributes
+#to just those listed in the slots list
+
+
+class LogEntry:
+    __slots__ = ("timestamp", "level", "message")
+
+    def __init__(self):
+        self.timestamp = None
+        self.level = None
+        self.message = None
+
+
+newLog = LogEntry()
+newLog.timestamp = "09:21"
+newLog.level = "INFO"
+newLog.message = "Here is a log message"
+
+print(newLog.timestamp)
+#newLog.commitMessage = "commit message"
